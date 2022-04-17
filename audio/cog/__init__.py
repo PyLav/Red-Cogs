30571ee2edacd747ff6ad357de9849cfbef67494
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-04-17 17:57:10 +0100

    rename attribute to lavalink

--- audio/cog/__init__.py
+++ audio/cog/__init__.py
@@ -11,36 +11,36 @@ class MediaPlayer(commands.Cog):
     def __init__(self, bot: Red):
         self.bot = bot
         try:
-            self.pylav: Client = Client(bot=bot, cog=self, config_folder=cog_data_path(raw_name="PyLav"))
+            self.lavalink: Client = Client(bot=bot, cog=self, config_folder=cog_data_path(raw_name="PyLav"))
         except (CogHasBeenRegistered, CogAlreadyRegistered):
-            self.pylav: Client = self.bot.pylav
+            self.lavalink: Client = self.bot.lavalink
 
     async def initialize(self):
-        if not self.bot.pylav.initialized:
-            await self.bot.pylav.initialize()
+        if not self.bot.lavalink.initialized:
+            await self.bot.lavalink.initialize()
 
     async def cog_unload(self) -> None:
         pass
-        #await self.bot.pylav.unregister(cog=self)
+        # await self.bot.lavalink.unregister(cog=self)
 
     @commands.command(name="play", aliases=["p"])
     @commands.guild_only()
     @commands.is_owner()
     async def command_play(self, ctx: commands.Context, *, query: QueryConverter):
         """Match query to a song and play it."""
-        if (player := self.pylav.get_player(ctx.guild)) is None:
-            player = await self.pylav.connect_player(channel=ctx.author.voice.channel)
+        if (player := self.lavalink.get_player(ctx.guild)) is None:
+            player = await self.lavalink.connect_player(channel=ctx.author.voice.channel)
 
-        tracks = await self.pylav.get_tracks(query)
+        tracks = await self.lavalink.get_tracks(query)
         if not tracks:
             return await ctx.send("No results found.")
         if query.is_single:
             tracks = [tracks["tracks"].pop(0)]
         else:
             tracks = tracks["tracks"]
-        await player.bulk_add(requester=ctx.author.id, tracks_and_queries=[
-            (track["track"], query) for track in tracks
-        ])
+        await player.bulk_add(requester=ctx.author.id, tracks_and_queries=[(track["track"], query) for track in tracks])
 
         if not player.is_playing:
             await player.play()
+
+        await ctx.tick(message=f"{len(tracks)} Tracks enqueued")
