9ee5a2e583f1fbb3258fd23fa20973a5c6c43c53
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-08-17 12:50:09 +0100

    rename `[p]playnow` tp `[p]bump`, add `[p]playnext`

--- audio/player_commands.py
+++ audio/player_commands.py
@@ -1,5 +1,6 @@
 import contextlib
 from abc import ABC
+from functools import partial
 from pathlib import Path
 
 import discord
@@ -11,6 +12,7 @@ from pylav import Query, Track
 from pylav.tracks import decode_track
 from pylav.types import PyLavCogMixin
 from pylav.utils import PyLavContext, format_time
+from pylavcogs_shared.utils import rgetattr
 from pylavcogs_shared.utils.decorators import invoker_is_dj
 
 LOGGER = getLogger("red.3pt.PyLavPlayer.commands.player")
@@ -18,10 +20,10 @@ _ = Translator("PyLavPlayer", Path(__file__))
 
 
 class PlayerCommands(PyLavCogMixin, ABC):
-    @commands.command(name="playnow", description="Plays the specified track in the queue.", aliases=["pn"])
+    @commands.command(name="bump", description="Plays the specified track in the queue.")
     @commands.guild_only()
     @invoker_is_dj()
-    async def command_playnow(self, context: PyLavContext, queue_number: int, after_current: bool = False):
+    async def command_bump(self, context: PyLavContext, queue_number: int, after_current: bool = False):
         """
         Plays the specified track in the queue.
 
@@ -91,6 +93,108 @@ class PlayerCommands(PyLavCogMixin, ABC):
                 ephemeral=True,
             )
             await player.play(track=track, requester=context.author, query=await track.query())
+
+    @commands.command(name="playnext", description="Enqueue a track a song to the top of the queue.", aliases=["pn"])
+    @commands.guild_only()
+    @invoker_is_dj()
+    async def command_playnext(self, context: PyLavContext, *, query: str):
+        """Enqueue a track a song to the top of the queue."""
+        if isinstance(context, discord.Interaction):
+            send = partial(context.followup.send, wait=True)
+            if not context.response.is_done():
+                await context.response.defer(ephemeral=True)
+            author = context.user
+        else:
+            send = context.send
+            author = context.author
+
+        player = context.player
+
+        if player is None:
+            config = await self.lavalink.player_config_manager.get_config(context.guild.id)
+            if (channel := context.guild.get_channel_or_thread(config.forced_channel_id)) is None:
+                channel = rgetattr(author, "voice.channel", None)
+                if not channel:
+                    await send(
+                        embed=await self.lavalink.construct_embed(
+                            description=_("You must be in a voice channel to allow me to connect."), messageable=context
+                        ),
+                        ephemeral=True,
+                    )
+                    return
+            if not (
+                (permission := channel.permissions_for(context.guild.me)) and permission.connect and permission.speak
+            ):
+                await send(
+                    embed=await self.lavalink.construct_embed(
+                        description=_("I don't have permission to connect or speak in {channel}.").format(
+                            channel=channel.mention
+                        ),
+                        messageable=context,
+                    ),
+                    ephemeral=True,
+                )
+                return
+            player = await self.lavalink.connect_player(channel=channel, requester=author)
+
+        queries = [await Query.from_string(qf) for q in query.split("\n") if (qf := q.strip("<>").strip())]
+        search_queries = [q for q in queries if q.is_search]
+        non_search_queries = [q for q in queries if not q.is_search]
+        total_tracks_enqueue = 0
+        single_track = None
+        if search_queries:
+            total_tracks_from_search = 0
+            for query in search_queries:
+                single_track = track = Track(node=player.node, data=None, query=query, requester=author.id)
+                await player.add(requester=author.id, track=track, index=0)
+                if not player.is_playing:
+                    await player.next(requester=author)
+                total_tracks_enqueue += 1
+                total_tracks_from_search += 1
+        if non_search_queries:
+            successful, count, failed = await self.lavalink.get_all_tracks_for_queries(
+                *non_search_queries, requester=author, player=player
+            )
+            if successful:
+                single_track = successful[0]
+            total_tracks_enqueue += count
+            failed_queries = []
+            failed_queries.extend(failed)
+            if count:
+                if count == 1:
+                    await player.add(requester=author.id, track=successful[0], index=0)
+                else:
+                    await player.bulk_add(requester=author.id, tracks_and_queries=successful, index=0)
+        if not (player.is_playing or player.queue.empty()):
+            await player.next(requester=author)
+
+        if total_tracks_enqueue > 1:
+            await send(
+                embed=await self.lavalink.construct_embed(
+                    description=_("{track_count} tracks enqueued.").format(track_count=total_tracks_enqueue),
+                    messageable=context,
+                ),
+                ephemeral=True,
+            )
+        elif total_tracks_enqueue == 1:
+            await send(
+                embed=await self.lavalink.construct_embed(
+                    description=_("{track} enqueued.").format(
+                        track=await single_track.get_track_display_name(with_url=True)
+                    ),
+                    thumbnail=await single_track.thumbnail(),
+                    messageable=context,
+                ),
+                ephemeral=True,
+            )
+        else:
+            await send(
+                embed=await self.lavalink.construct_embed(
+                    description=_("No tracks were found for your query."),
+                    messageable=context,
+                ),
+                ephemeral=True,
+            )
 
     @commands.command(name="remove", description="Remove the specified track from the queue.")
     @commands.guild_only()
