96ba302507e0dfff425034b1a58583874e67359d
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-09-03 00:03:39 +0100

    PyLavConfigurator tested

--- plconfig/cog.py
+++ plconfig/cog.py
@@ -34,7 +34,7 @@ class PyLavConfigurator(commands.Cog):
 
     lavalink: Client
 
-    __version__ = "1.0.0.0rc0"
+    __version__ = "1.0.0.0rc1"
 
     def __init__(self, bot: BotT, *args, **kwargs):
         super().__init__(*args, **kwargs)
